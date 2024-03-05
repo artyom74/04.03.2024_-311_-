b1a79a65ed9d22aca200129e776dabef132c13c9
artyom74 <artemterentev66@gmail.com>
2024-03-06 00:51:45 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+float calc_side(float r) {
+
+    float pi = 3.14;
+
+    cout << "Сторона квадрата: ";
+
+    return r * sqrt(pi);
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    float radius;
+    cout << "Введите радиус круга:" << endl;
+    cin >> radius;
+
+    cout << calc_side(radius);
+
+    return 0;
+}
+
+/* Написать функцию, которая получает на вход радиус круга,
+а возвращает сторону квадрата, равного этому кругу по площади.
+Вызвать функцию в main, результат вывести тоже в main.*/
